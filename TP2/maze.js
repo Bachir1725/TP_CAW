82b41b80909ec5f57b8b4a6a0f12ae2f8ac9d2b2
Bachir1725 <96068667+Bachir1725@users.noreply.github.com>
2022-10-11 00:11:17 +0200

    last

--- TP2/maze.js
+++ TP2/maze.js
@@ -1,34 +1,80 @@
-
-
-import EX3 from "./EX03.js";
-import { EX1 } from "./EXO1.js";
-export{
-  endGame,islost,btn,maze,end,iswinner,passStart,example
-}
+"use strict";
 const maze = document.querySelectorAll(".boundary");
+const p = document.querySelectorAll("p");
 const example = document.querySelector(".example");
 const end = document.querySelector("#end");
 const start = document.querySelector("#start");
 const btn = document.querySelector(".example");
+const stat = document.querySelector("#status");
 const body = document.querySelector("body");
-
 var islost = false;
 var iswinner = false;
 var passStart = false;
 var endGame = false;
+var startX;
 
-if (islost || iswinner) {
-  
-}else{
-  start.addEventListener("mouseover", function () {
-    if (iswinner == false && islost == false && endGame == false) {
-      passStart = true;
-    }
-    if (passStart) {
-      //  start EX1
-  EX1();
-  // EX3;
-  EX3();
+start.addEventListener("mouseover", function () {
+  if (iswinner == false && islost == false && endGame == false) {
+    passStart = true;
+  }
+  if (passStart) {
+    
+maze.forEach((bo) => {
+  // EX1 + EX2
+  bo.addEventListener("mouseenter", function () {
+    if (endGame == false) {
+      bo.style.backgroundColor = "red";
+      setTimeout(() => {
+        maze.forEach(element => {
+          element.style.backgroundColor = "red";
+          btn.style.backgroundColor = "";
+        });
+       
+      }, 1000);
+      islost = true;
+      endGame = true;
+      stat.textContent = "LOST !!";
     }
   });
-}
+});
+//  EX3
+end.addEventListener("mouseover", function () {
+  if (
+    iswinner == false &&
+    islost == false &&
+    passStart == true &&
+    endGame == false
+  ) {
+    //  EX 6
+    stat.textContent = "WIN !!";
+    endGame = true;
+  }
+  iswinner = true;
+
+});
+ // EX 4
+start.addEventListener("click",function(){
+  window.location.reload();
+})
+
+  }
+});
+// EX 7
+body.addEventListener("mouseover", function (e) {
+  const limitMouse=e.clientX;
+   startX = start.getBoundingClientRect().left;
+    
+  //  console.log(startX,limitMouse,endGame,passStart);
+ 
+   if (limitMouse < startX  && passStart && endGame == false) {
+     maze.forEach(element => {
+       element.style.backgroundColor = "red";
+       btn.style.backgroundColor = "";
+     });
+ 
+     islost = true;
+     endGame = true;
+     stat.textContent = "LOST !!";
+   }
+ });
+
